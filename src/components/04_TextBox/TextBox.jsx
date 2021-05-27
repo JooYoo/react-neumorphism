661efad4e826be2006bc59dc53091574a37cae79
jooyoo <jooyoo@outlook.com>
2021-05-27 05:17:06 +0200

    feat: textbox valid and invalid

--- src/components/04_TextBox/TextBox.jsx
+++ src/components/04_TextBox/TextBox.jsx
@@ -2,26 +2,28 @@ import React from 'react';
 import style from './TextBox.module.scss';
 
 const TextBox = (props) => {
-  const { label, placeholder, description, textarea, valid } = props;
+  const { label, placeholder, description, textarea, valid, invalid } = props;
+
+  // var
+  const isValid = valid ? 'textbox--valid' : '';
+  const isInvalid = invalid ? 'textbox--invalid' : '';
 
   return (
     <div className={style.host}>
       <label className={style.label}>{label}</label>
 
-      <div>
-        {textarea ? (
-          <textarea
-            className={style.textarea}
-            placeholder={placeholder}
-          ></textarea>
-        ) : (
-          <input
-            className={style['input-box']}
-            type="textarea"
-            placeholder={placeholder}
-          />
-        )}
-      </div>
+      {textarea ? (
+        <textarea
+          className={style.textarea}
+          placeholder={placeholder}
+        ></textarea>
+      ) : (
+        <input
+          className={`${style['input-box']} ${style[isValid]} ${style[isInvalid]}`}
+          type="text"
+          placeholder={placeholder}
+        />
+      )}
 
       {description ? (
         <div className={style.description}>{description}</div>
